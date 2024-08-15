5014685c772a60338beca15e887cbd749f62f820
VladyslavPilkevych <90135414+VladyslavPilkevych@users.noreply.github.com>
2024-08-15 19:00:15 +0200

    feat: created new utils functions for getting chracters and episodes

---
+++ src/utils/fetchCharacterWithEpisodes.ts
@@ -0,0 +1,29 @@
+import { ICharacterData } from '../types';
+import { fetchCharacterById } from './fetchCharacterById';
+import { fetchMultiplyEpisodesByIds } from './fetchMultiplyEpisodesByIds';
+
+interface IEpisodesResponse {
+  id: number;
+  name: string;
+}
+
+export const fetchCharacterWithEpisodes = async (
+  id: number
+): Promise<{ character: ICharacterData; episodes: IEpisodesResponse[] }> => {
+  try {
+    const characterData = await fetchCharacterById(id);
+
+    const episodeIds =
+      characterData.episode?.map((url) => Number(url.split('/').pop())) ?? [];
+
+    const episodesData = await fetchMultiplyEpisodesByIds(episodeIds);
+
+    return {
+      character: characterData,
+      episodes: episodesData?.map((episode) => ({id: episode.id, name: episode?.name})),
+    };
+  } catch (error) {
+    console.error('Error fetching character with episodes:', error);
+    throw error;
+  }
+};
